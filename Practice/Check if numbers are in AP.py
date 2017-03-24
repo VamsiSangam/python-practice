80d68e6bc8beec94e8163396e6ea26053b73717e
Matthew Everson <ThomasMatthewEvans@users.noreply.github.com>
2017-03-24 20:37:16 +0000

    Create Check if numbers are in AP.py

---
+++ Practice/Check if numbers are in AP.py
@@ -0,0 +1,10 @@
+# Authored by - Matthew Everson, https://github.com/ThomasMatthewEvans
+
+Number1 = int(input("Enter a number:"))
+Number2 = int(input("Enter a second number:"))
+Number3 = int(input("Enter a third number:"))
+
+if Number3 == (Number2 - Number1) + Number2:
+    print ("Yes")
+else:
+    print("No")
